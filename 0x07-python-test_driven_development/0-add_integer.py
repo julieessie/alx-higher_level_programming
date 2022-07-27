286be9a703a80fbd151c7df7c8ef6c3f1996f341
julieessie <julienyambura50@gmail.com>
2022-07-27 00:22:51 +0000

    tasks

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to add two integers."""
+
+
+def add_integer(a, b=98):
+    """Adds integers and b
+    Raises:
+        TypeError: if a, b are neither int nor float.
+    Returns:
+        sum of the 2 integers.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
